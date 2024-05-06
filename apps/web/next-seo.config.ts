f77906ab4423f69ef542978cd65bf299c61d3185
Hamdan-Khan <hamdankhan212@gmail.com>
2024-05-07 01:30:51 +0500

    Preview Meta image changed

--- apps/web/next-seo.config.ts
+++ apps/web/next-seo.config.ts
@@ -12,6 +12,6 @@ export const SEO: DefaultSeoProps = {
   openGraph: {
     title: "🥞 OpenWorldSwap - Everyone's Favorite DEX",
     description: 'Trade, earn, and own crypto on the all-in-one multichain DEX',
-    images: [{ url: 'https://assets.pancakeswap.finance/web/og/v2/hero.jpg' }],
+    images: [{ url: 'https://app.openworldswap.finance/logo.png' }],
   },
 }
