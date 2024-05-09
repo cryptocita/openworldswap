14709d0f916f0bf0aa96834c4b62887f43e2e297
Hamdan-Khan <hamdankhan212@gmail.com>
2024-05-10 00:15:06 +0500

    Swap Confirmation Pancake image removed

--- packages/widgets-internal/swap/ConfirmationPendingContent.tsx
+++ packages/widgets-internal/swap/ConfirmationPendingContent.tsx
@@ -1,6 +1,6 @@
 import { useTranslation } from "@pancakeswap/localization";
+import { AutoColumn, Box, ColumnCenter, Text } from "@pancakeswap/uikit";
 import { styled } from "styled-components";
-import { Spinner, Text, ColumnCenter, AutoColumn } from "@pancakeswap/uikit";
 
 const Wrapper = styled.div`
   width: 100%;
@@ -12,10 +12,13 @@ const ConfirmedIcon = styled(ColumnCenter)`
 
 export function ConfirmationPendingContent({ pendingText }: { pendingText?: string }) {
   const { t } = useTranslation();
+  const size = 128;
   return (
     <Wrapper>
       <ConfirmedIcon>
-        <Spinner />
+        <Box width={size} height={size} position="relative">
+          <img width={size} height={size} src="/logo.png" alt="openworldswap-loader" />
+        </Box>
       </ConfirmedIcon>
       <AutoColumn gap="12px" justify="center">
         {pendingText ? (
