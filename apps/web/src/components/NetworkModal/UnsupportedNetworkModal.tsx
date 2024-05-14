8097bf2bf73a3e0273f0b38709f4152eb43dcf40
Hamdan-Khan <hamdankhan212@gmail.com>
2024-05-14 22:45:07 +0500

    Unsupported networks image updated

--- apps/web/src/components/NetworkModal/UnsupportedNetworkModal.tsx
+++ apps/web/src/components/NetworkModal/UnsupportedNetworkModal.tsx
@@ -1,16 +1,16 @@
-import { Button, Grid, Message, MessageText, Modal, Text } from '@pancakeswap/uikit'
-import { useLocalNetworkChain } from 'hooks/useActiveChainId'
+import { ChainId } from '@pancakeswap/chains'
 import { useTranslation } from '@pancakeswap/localization'
+import { Button, Grid, Message, MessageText, Modal, Text } from '@pancakeswap/uikit'
+import { useMenuItems } from 'components/Menu/hooks/useMenuItems'
+import { getActiveMenuItem, getActiveSubMenuItem } from 'components/Menu/utils'
+import { useLocalNetworkChain } from 'hooks/useActiveChainId'
+import useAuth from 'hooks/useAuth'
 import { useSwitchNetwork, useSwitchNetworkLocal } from 'hooks/useSwitchNetwork'
 import Image from 'next/image'
-import useAuth from 'hooks/useAuth'
-import { useMenuItems } from 'components/Menu/hooks/useMenuItems'
 import { useRouter } from 'next/router'
-import { getActiveMenuItem, getActiveSubMenuItem } from 'components/Menu/utils'
-import { useAccount } from 'wagmi'
 import { useMemo } from 'react'
-import { ChainId } from '@pancakeswap/chains'
 import { viemClients } from 'utils/viem'
+import { useAccount } from 'wagmi'
 import Dots from '../Loader/Dots'
 
 // Where chain is not supported or page not supported
@@ -43,17 +43,11 @@ export function UnsupportedNetworkModal({ pageSupportedChains }: { pageSupported
     <Modal title={t('Check your network')} hideCloseButton headerBackground="gradientCardHeader">
       <Grid style={{ gap: '16px' }} maxWidth="336px">
         <Text>
-          {t('Currently %feature% only supported in', { feature: typeof title === 'string' ? title : 'this page' })}{' '}
+          {t('Currently %feature% is only supported in', { feature: typeof title === 'string' ? title : 'this page' })}{' '}
           {supportedMainnetChains?.map((c) => c?.name).join(', ')}
         </Text>
         <div style={{ textAlign: 'center' }}>
-          <Image
-            layout="fixed"
-            width={194}
-            height={175}
-            src="/images/check-your-network.png"
-            alt="check your network"
-          />
+          <Image layout="fixed" width={194} height={175} src="/logo.png" alt="check your network" />
         </div>
         <Message variant="warning">
           <MessageText>{t('Please switch your network to continue.')}</MessageText>
